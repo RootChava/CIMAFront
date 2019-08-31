fe82e6ca5c12edd132b301bb11bd325614733516
Juan Salvador Cortes Francisco <Chava@localhost.localdomain>
2019-08-30 21:35:38 -0500

    Se modifico el llamado a las instancias

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Col from "react-bootstrap/Col";
 import RootImage from "./images/root.png";
 import "./header.css";
 
-export default class Header extends Component {
-  render() {
+const Header = () => {
     return (
-      <>
+        <>
         <Col xs={12} className="no-padding vert-margin-soft">
           <p className="text-center">
             <img className="max-300" alt="Root Logo" src={RootImage} />
@@ -16,6 +15,7 @@ export default class Header extends Component {
             <h5 className="text-center"> </h5>
         </Col>
       </>
-    );
-  }
-}
+    )
+};
+
+export default Header;
